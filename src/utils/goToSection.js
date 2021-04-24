491dd78bea20cd327f6ff5208aa2bbd54a4c1f16
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 14:42:07 +0000

    goToSection.js: changed

--- src/utils/goToSection.js
+++ src/utils/goToSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function GoToSection({section}) {
+export default function goToSection({section}) {
     // Pass Any internal link to Next.js Link, for anything else, use <a> tag
     
     return <a href={href} {...other}>{children}</a>;
