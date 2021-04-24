9667b8c43b384800d68b680c25c891bd5cd3ac71
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 14:47:15 +0000

    goToSection.js: changed

--- src/utils/goToSection.js
+++ src/utils/goToSection.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default function goToSection({ section}) {
-    const internal = /^\/(?!\/)/.test(href);
 
-
+    
     return <a href={href} {...other}>{children}</a>;
 }
