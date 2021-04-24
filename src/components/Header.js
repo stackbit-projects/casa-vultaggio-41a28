eaf17e028ff0fad70ce3309e7702ad4616a7e6b8
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 14:46:02 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,7 @@ export default class Header extends React.Component {
                                 let isActive = (_.get(item, 'title', null) === section) ? (true) : false;
                                 return (<React.Fragment key={item_idx + '.1'}>
                                     <li key={item_idx} className="nav__menu-item">
-                                        <GoToSection href={withPrefix(_.get(item, 'url', null))} className={classNames('nav__menu-item-link', {'nav__menu-item-link--active': isActive})}>
+                                        <goToSection href={withPrefix(_.get(item, 'url', null))} className={classNames('nav__menu-item-link', {'nav__menu-item-link--active': isActive})}>
                                             {_.get(item, 'title', null)}
                                         </GoToSection>
                                     </li>
