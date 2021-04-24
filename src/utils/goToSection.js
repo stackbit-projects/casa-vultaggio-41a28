4d825701393f4dfbb6410745983b7b6170dafad9
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 14:02:56 +0000

    goToSection.js: changed

--- src/utils/goToSection.js
+++ src/utils/goToSection.js
@@ -2,17 +2,6 @@ import React from 'react';
 
 export default function GoToSection({section}) {
     // Pass Any internal link to Next.js Link, for anything else, use <a> tag
-    const internal = /^\/(?!\/)/.test(href);
-
-    if (internal) {
-        // For root page, use index.js, for rest use [...slug].js
-        const page = href === '/' ? '/' : '/[...slug]';
-        return (
-            <NextLink href={page} as={href}>
-                <a {...other}>{children}</a>
-            </NextLink>
-        );
-    }
-
+    
     return <a href={href} {...other}>{children}</a>;
 }
